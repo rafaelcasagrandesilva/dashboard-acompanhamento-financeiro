5113da1fc07f5bd924bd701416c43f978f52e0bd
rafaelcasagrandesilva <rafaelcasagrandesilva@gmail.com>
2026-01-29 16:31:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import base64
 
 # ================= PAGE CONFIG =================
 st.set_page_config(
-    page_title="Acompanhamento Financeiro – Diretoria",
+    page_title="Dash Financ",
     page_icon="📈",
     layout="wide",
     initial_sidebar_state="collapsed"
